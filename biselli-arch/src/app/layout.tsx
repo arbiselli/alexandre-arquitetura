afa47dfa9950636b05b0f49176c37bc93b1bfd49
Felipe Fioravanti <felipefioravanti@live.com>
2024-11-21 11:26:11 -0300

    add loading.tsx

--- biselli-arch/src/app/layout.tsx
+++ biselli-arch/src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <main className={styles.main}>{children}</main>
         </body>
       </ColorProvider>
-      <Suspense>{children}</Suspense>
     </html>
   );
 }
